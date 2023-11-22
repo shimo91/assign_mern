86ae78c51cfa4144829421de45c7619615ef7e96
Shimona <141317393+shimo91@users.noreply.github.com>
2023-11-22 20:24:01 +0400

    Update axiosinterceptor.js
    https://employee-mern-back-app.onrender.com/ instead localhost

--- frontend/src/ui-components/axiosinterceptor.js
+++ frontend/src/ui-components/axiosinterceptor.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const axiosInstance = axios.create({
-    baseURL:'http://127.0.0.1:5000'
+    baseURL:'https://employee-mern-back-app.onrender.com'
 });
 
 //Request Interptor
